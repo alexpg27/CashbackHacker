a4120712f3be627453688cae0481ae35c28b3fe9
Alex Makarov <alexpg27@yandex.ru>
2021-05-04 13:57:51 +0300

    Add junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,13 +1,43 @@
 package ru.netology.service;
 
 
-import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService cashbackHackService = new CashbackHackService();
 
-    @Test
+    @org.junit.jupiter.api.Test
+    public void userPurchaseInTheAmountOf900Jupiter() {
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void userPurchaseInTheAmountOf1000Jupiter() {
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void userPurchaseInTheAmountOf1500Jupiter() {
+        int amount = 1500;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 500;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
     public void userPurchaseInTheAmountOf900() {
         int amount = 900;
 
@@ -17,7 +47,7 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void userPurchaseInTheAmountOf1000() {
         int amount = 1000;
 
@@ -27,7 +57,7 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void userPurchaseInTheAmountOf1500() {
         int amount = 1500;
 
